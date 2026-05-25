a9682cb87680fb32c5a656a422be3df75e23d27f
Josefa <jose.cruces@duocuc.cl>
2026-05-25 00:34:27 -0400

    microservicio_sucursal

---
+++ src/main/java/com/sucursal/Microservicio_sucursal/MicroservicioSucursalApplication.java
@@ -0,0 +1,13 @@
+package com.sucursal.Microservicio_sucursal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioSucursalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioSucursalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sucursal/Microservicio_sucursal/MicroservicioSucursalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sucursal.Microservicio_sucursal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioSucursalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
